5cc28d7efda364851b40624d4ae413448eec4c62
U-TIEN-PC\Tien Tran <ttran088@ucr.edu>
2016-02-24 02:38:31 -0800

    fixed some bugs

--- assn3/assn3.cpp
+++ assn3/assn3.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <pthread.h>
 #include <ctime>
+#include <math.h>
 
 using namespace std;
 #define cols 1000
 #define rows 1000
+#define rowsPerThread 100
+#define numThreads 10
 
 int MAT1[cols][rows];
 int MAT2[cols][rows];
@@ -14,8 +17,17 @@ int resThr[cols][rows];
 
 void *threadMultMat(void *p);
 
+void printMat(int mat[rows][cols]){
+	int x,y;
+	for(x = 0; x < rows; x++)
+		for(y = 0; y < cols; y++){
+			cout << " " << mat[x][y];
+			if(y==cols-1)
+				cout << endl;
+		}
+}
 int main(){
-	clock_t start;
+	struct timespec start, finish;
 	double endSeq, endThr;
 	//Initialize Matrixes for square matrices
 	int x,y;
@@ -37,87 +49,69 @@ int main(){
 			cout << "Enter value for M2[" << x << "][" << y << "]: ";
 			cin >> MAT2[x][y];
 			*/
-			MAT1[x][y] = 5;
+			MAT2[x][y] = 5;
 		}
-
-	/*
-	//Output M1
-	cout << "Matrix 1: " << endl;
-	for(x = 0; x < rows; x++)
-		for(y = 0; y < cols; y++){
-			cout << " " << MAT1[x][y];
-			if(y==cols-1)
-				cout << endl;
-		}
-
-	//Output M2
-	cout << "Matrix 2: " << endl;
-	for(x = 0; x < rows; x++)
-		for(y = 0; y < cols; y++){
-			cout << " " << MAT2[x][y];
-			if(y==cols-1)
-				cout << endl;
-		}
-	*/
-	start = clock();
 
 	//Sequential
 	int i,j,k;
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	for(i = 0; i < rows; i++)
 		for(j = 0; j < cols; j++)
 			for(k = 0; k < cols; k++){
 				resSeq[i][j] += MAT1[i][k] * MAT2[k][j];
 			}
 
-	endSeq = (clock() - start) / (double) CLOCKS_PER_SEC;
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	endSeq = (finish.tv_sec - start.tv_sec);
+	endSeq += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-	/*
-	//Output result of sequential multiplication
-	cout << "Seq Res: " << endl;
-	for(x = 0; x < rows; x++)
-		for(y = 0; y < cols; y++){
-			cout << " " << resSeq[x][y];
-			if(y==cols-1)
-				cout << endl;
-		}
-	*/
 	cout << "Sequential done" << endl;
 
-	start = clock();
+
 	//Parralel/Threads
-	pthread_t tid;
-	pthread_create(&tid, NULL, threadMultMat, NULL);
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	pthread_t tid[numThreads];
 
-	for(i = 0; i < rows; i = i + 2)
-		for(j = 0; j < cols; j++)
-			for(k = 0; k < cols; k++){
-				resThr[i][j] += MAT1[i][k] * MAT2[k][j];
-			}
+    for (j=0;j<numThreads;j++)
+        pthread_create(&tid[j],NULL,threadMultMat,(void*)j);
 
-	endThr = (clock() - start) / (double) CLOCKS_PER_SEC;
+    for (j=0;j<numThreads;j++)
+        pthread_join(tid[j],NULL);
 
-	/*
-	//Output thread/Parrallel matrix
-	cout << "Parrallel Res: " << endl;
-	for(x = 0; x < rows; x++)
-		for(y = 0; y < cols; y++){
-			cout << " " << resThr[x][y];
-			if(y==cols-1)
-				cout << endl;
-		}
-	*/
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+	endThr = (finish.tv_sec - start.tv_sec);
+	endThr += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
 	cout << "Parrallel done" << endl;
 
+	//Output times
 	cout << "Seq: " << endSeq << " Thr: " << endThr << endl;
+	double maxDiff=0; 
+	/*
+	printMat(resSeq);
+	cout << endl;
+	printMat(resThr);
+	*/
+	cout << endl;
+	for(i=0; i<rows; i++){
+		for(j = 0; j < cols; j++){
+			if(fabs(resThr[i][j]-resSeq[i][j])>maxDiff) 
+				maxDiff=fabs(resThr[i][j]-resSeq[i][j]); 
+		}
+	} 
+	cout << "MaxDiff = " << maxDiff << endl;
 	return 0;
 }
 
-void *threadMultMat(void *p){
-	int i,j,k;
-	for(i = 1; i < rows; i = i + 2)
-		for(j = 0; j < cols; j++)
-			for(k = 0; k < cols; k++){
-				resThr[i][j] += MAT1[i][k] * MAT2[k][j];
-			}
-	pthread_exit(NULL);
-}
+void *threadMultMat(void *t)
+{   
+    int startRow;
+    startRow = (intptr_t)t;
+    startRow = rowsPerThread * startRow;
+
+    int i,j,k;
+    for (i = startRow;i<startRow + rowsPerThread;i++)
+        for (j=0;j<cols;j++)
+            for (k=0;k<rows;k++)
+                resThr[i][j] += (MAT1[i][k] * MAT2[k][j]);
+ }
